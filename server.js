9fddbd4f87c3ea3bf0c2ac5a4ce0fcd8c7ed8b2c
AJTECH <145960311+AJTECH0001@users.noreply.github.com>
2025-02-14 10:05:20 +0100

    updating my repo

--- server.js
+++ server.js
@@ -4,12 +4,30 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
-    id: 1,
+const friends = [
+  {
+    id: 0,
     name: 'John Doe',
-  });
+  },
+  {
+    id: 1,
+    name: 'Doe John',
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
 })
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({error: "Friend does not exist"}); 
+  }
+});
 
 app.get('/messages', (req, res) => {
   res.send('<ul><li>Message 1</li><li>Message 2</li></ul>');
