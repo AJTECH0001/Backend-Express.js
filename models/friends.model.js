ee297d19302c8df8be287a87a996e0522d83094f
AJTECH <145960311+AJTECH0001@users.noreply.github.com>
2025-02-18 14:28:32 +0100

    added routes and controller

---
+++ models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+  {
+    id: 0,
+    name: 'John Doe Mike',
+  },
+  {
+    id: 1,
+    name: 'Doe John Judge',
+  }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,64 +1,25 @@
-const express = require('express');
+const express = require("express");
+
+const friendsRouter = require("./routes/friends.router");
+
+const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-  {
-    id: 0,
-    name: 'John Doe Mike',
-  },
-  {
-    id: 1,
-    name: 'Doe John Judge',
-  }
-];
-
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} request for ${req.url}${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl} ${req.url}${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends', (req, res) => {
+app.use("/friends", friendsRouter);
 
-  if (!req.body.name) {
-    res.status(400).json({error: "Name is required"});
-  }
-  const newFriend = {
-    name: req.body,
-    id: friends.length
-  };
-  friends.push(newFriend);
-
-  res.json(newFriend); 
-})
-
-app.get('/friends', (req, res) => {
-  res.json(friends);
-})
-
-app.get('/friends/:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({error: "Friend does not exist"}); 
-  }
-});
-
-app.get('/messages', (req, res) => {
-  res.send('<ul><li>Message 1</li><li>Message 2</li></ul>');
-})
-
-app.post('/', (req, res) => {
-  res.send('Upddating messages...');
-})
+app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
